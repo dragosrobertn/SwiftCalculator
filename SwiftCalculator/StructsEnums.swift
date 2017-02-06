a87c0516f34dd8a03de6d37f3fa6df4bca8be754
dragosrobertn <hello@dragosneagu.com>
2017-02-06 15:56:36 +0000

    Initial commit with button images and simple tap functionality :zap:

---
+++ SwiftCalculator/StructsEnums.swift
@@ -0,0 +1,22 @@
+//
+//  StructsEnums.swift
+//  SwiftCalculator
+//
+//  Created by Dragos Neagu on 06/02/2017.
+//  Copyright © 2017 Dragos Neagu. All rights reserved.
+//
+
+import Foundation
+
+enum Operator: String {
+    case add = "+"
+    case substract = "-"
+    case multiply = "*"
+    case divide = "/"
+    case nothing = ""
+}
+
+enum CalculationState : String {
+    case enterinNum = "enteringNum"
+    case newNumStarted = "newNumStarted"
+}
--- SwiftCalculator/ViewController.swift
+++ SwiftCalculator/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentOperation : Operator = Operator.nothing
+    var calcState : CalculationState = CalculationState.newNumStarted
+    
+    var firstValue : String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,44 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func numberClicked(sender: UIButton) {
+        //updateDisplay(String(sender.tag))
+    }
+    
+    @IBAction func operatorClicked(sender: UIButton) {
+        switch sender.tag {
+        case 10:
+            print ("Add")
+        case 11:
+            print ("Substract")
+        case 12:
+            print ("Multiply")
+        case 13:
+            print ("Divide")
+        default:
+            return
+        }
+    }
+    
+    @IBAction func equalsClicked(sender: UIButton) {
+        
+    }
+    
+    func updateDisplay(number: String){
+//        if calcState == CalculationState.newNumStarted {
+//            if let num = resultLabel.text {
+//                if num != "" {
+//                    firstValue = num
+//                }
+//            }
+//            calcState = CalculationState.enterinNum
+//            resultLabel.text = number
+//        }
+//        else if calcState == CalculationState.enterinNum {
+//            resultLabel.text = resultLabel.text! + number
+//        }
     }
 
 
